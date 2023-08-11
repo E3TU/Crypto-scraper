9e14715b4caaf2f6c3debd60a6c23499ee4de728
E3TU <yes.0ztan@slmail.me>
2023-08-11 23:58:03 +0300

    Get the usd eur exchange rate

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
 import fetch from "node-fetch";
 import * as cheerio from 'cheerio';
 
+let i;
+let links = [];
+let classes = [];
+
 const url = "https://coinmarketcap.com/currencies/monero/";
-const USD_EUR_RATE = "https://www.investing.com/currencies/usd-eur";
+const usd_eur = "https://fi.investing.com/currencies/usd-eur";
 
-async function getEuroDollarExchangeRate() {
+const cryptopriceEl = "span.sc-16891c57-0.dxubiK.base-text";
+const usd_eurpriceEl = "span.text-2xl";
 
-}
+links.push(url, usd_eur);
+classes.push(cryptopriceEl, usd_eurpriceEl);
 
 async function getcryptoPrice() {
-    const res = await fetch(url);
-    const html = await res.text();
-
-    const $ = cheerio.load(html);
-
-    const priceEl = $("span.sc-16891c57-0.dxubiK.base-text");
-
-    const price = priceEl.text().trim();
-
-    console.log("Monero price: " + price);
+    for(i = 0; i < 2; i++) {
+        const res = await fetch(links[i]);
+        const html = await res.text();
+    
+        const $ = cheerio.load(html);
+    
+        const priceEl = $(classes[i]);
+    
+        const price = priceEl.text().trim();
+    
+        console.log(price);
+    }
 }
-
 
 getcryptoPrice();
