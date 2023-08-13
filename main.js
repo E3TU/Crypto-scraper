0d70e07b483119b855eff89b8f1aa55c0f5d4f58
E3TU <yes.0ztan@slmail.me>
2023-08-13 18:35:32 +0300

    Update

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
 import fetch from "node-fetch";
-import * as cheerio from 'cheerio';
-
-let i;
-let links = [];
-let classes = [];
+import * as cheerio from "cheerio";
 
 const url = "https://coinmarketcap.com/currencies/monero/";
-const usd_eur = "https://fi.investing.com/currencies/usd-eur";
+const usd_eur = "https://www.investing.com/currencies/usd-eur";
 
 const cryptopriceEl = "span.sc-16891c57-0.dxubiK.base-text";
-const usd_eurpriceEl = "span.text-2xl";
-
-links.push(url, usd_eur);
-classes.push(cryptopriceEl, usd_eurpriceEl);
+const usdeurpriceEl = "span.text-2xl";
 
 async function getcryptoPrice() {
-    for(i = 0; i < 2; i++) {
-        const res = await fetch(links[i]);
-        const html = await res.text();
-    
-        const $ = cheerio.load(html);
-    
-        const priceEl = $(classes[i]);
-    
-        const price = priceEl.text().trim();
-    
-        console.log(price);
-    }
+  //Get price of the crypto
+  const res = await fetch(url);
+  const html = await res.text();
+  const $ = cheerio.load(html);
+  const priceEl = $(cryptopriceEl);
+  const price = priceEl.text().trim();
+  const parsedPrice = price.replace('$', '');
+
+  //Get usd eur exchange rate
+  const exchangerateRes = await fetch(usd_eur);
+  const exchangerateHtml = await exchangerateRes.text();
+  const exchangeRate$ = cheerio.load(exchangerateHtml);
+  const exchangerateEl = exchangeRate$(usdeurpriceEl);
+  const usdeurexchangeRate = parseFloat(exchangerateEl.text().trim());
+
+  const priceinEur = parsedPrice * usdeurexchangeRate;
+
+  console.log(parsedPrice);
+  console.log(usdeurexchangeRate);
+
+  console.log("Monero Price in EUR: " + priceinEur.toFixed(2));
 }
 
 getcryptoPrice();
